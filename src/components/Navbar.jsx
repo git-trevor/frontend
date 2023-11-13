455128817102119b95be48136c89913fae8a4fae
git-trevor <daniel.guerrero@outlook.com>
2023-11-12 20:11:27 -0600

    Commit con ajustes a Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom"
 import { useAccess } from "../context/AccessContext"
+import { Fragment } from "react";
+import { Menu, Transition } from '@headlessui/react'
+import { IoCaretDownOutline } from 'react-icons/io5'
+import { MdPets, MdGames, MdLogout } from "react-icons/md";
+
+function classNames(...classes){
+    return classes.filter(Boolean).join(' ')
+}
 
 function Navbar(){
     const { isAuthenticated, logout, user } = useAccess();
@@ -15,21 +23,94 @@ function Navbar(){
                 {
                     isAuthenticated ? (
                         <>
-                            <li className="font-Caveat text-xl">
-                                Bienvenido {user.username}
+                            <li className="font-Caveat text-3xl">
+                                {/* Bienvenido {user.username} */}
+                                Bienvenido
                             </li>
-                            <li>
+                            <Menu as="div" className="relative inline-block text-left">
+                                <div>
+                                    <Menu.Button
+                                        className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-sky-100 px-3 py-2 font-Caveat text-base ring-0 ring-inset ring-cyan-600 hover:bg-cyan-600 hover:text-white">
+                                        {/* <IoBagSharp className="-mr-1 h-5 w-5" /> */}
+                                        {user.username}
+                                        <IoCaretDownOutline className="-mr-1 h-5 w-5 text-cyan-600 hover:text-white" aria-hidden="true" />
+                                    </Menu.Button>
+                                </div>
+                                <Transition
+                                    as={Fragment}
+                                    enter="transition ease-out duration-100"
+                                    enterFrom="transform opacity-0 scale-95"
+                                    enterTo="transform opacity-100 scale-100"
+                                    leave="transition ease-in duration-75"
+                                    leaveFrom="transform opacity-100 scale-100"
+                                    leaveTo="transform opacity-0 scale-95"
+                                >
+                                    <Menu.Items
+                                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-sky-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    >
+                                        <div className="py-1">
+                                            <Menu.Item>
+                                                {
+                                                    ({ active }) => (
+                                                        <Link
+                                                            to='/pets'
+                                                            className={classNames(
+                                                                active ? 'bg-sky-200 font-Caveat text-xl' : 'font-Caveat text-xl',
+                                                                'block px-4 py-2 text-sm'
+                                                            )}
+                                                        >
+                                                        <MdPets className="h-5 w-8 inline-flex" />Mis Mascotas
+                                                        </Link>
+                                                    )
+                                                }
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {
+                                                    ({ active }) => (
+                                                        <Link
+                                                            to='/add-pet'
+                                                            className={classNames(
+                                                                active ? 'bg-sky-200 font-Caveat text-xl' : 'font-Caveat text-xl',
+                                                                'block px-4 py-2 text-sm'
+                                                            )}
+                                                        >
+                                                        <MdGames className="h-5 w-8 inline-flex" />Agregar Mascota
+                                                        </Link>
+                                                    )
+                                                }
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {
+                                                    ({ active }) => (
+                                                        <Link
+                                                            onClick={()=>{logout()}}
+                                                            to='/'
+                                                            className={classNames(
+                                                                active ? 'bg-sky-200 font-Caveat text-xl' : 'font-Caveat text-xl',
+                                                                'block px-4 py-2 text-sm'
+                                                            )}
+                                                        >
+                                                        <MdLogout className="h-5 w-8 inline-flex" />Cerrar Sesión
+                                                        </Link>
+                                                    )
+                                                }
+                                            </Menu.Item>
+                                        </div>
+                                    </Menu.Items>
+                                </Transition>
+                            </Menu>
+                            {/* <li>
                                 <Link to='/add-pet'
                                     className="bg-transparent hover:bg-cyan-600 text-sky-500
                                     font-semibold hover:text-white mb-2 py-1 px-2 border border-cyan-600 hover:border-transparent rounded"
                                 >Agregar Mascota</Link>
-                            </li>
-                            <li>
+                            </li> */}
+                            {/* <li>
                                 <Link to='/'
                                     className="bg-transparent hover:bg-cyan-600 text-sky-500
                                     font-semibold hover:text-white mb-2 py-1 px-2 border border-cyan-600 hover:border-transparent rounded"
                                     onClick={()=>{logout()}}>Cerrar Sesión</Link>
-                            </li>
+                            </li> */}
                         </>
                     ) : (
                         <>
--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -6,6 +6,14 @@ import { usePets } from '../context/PetsContext';
 
 import { Link } from 'react-router-dom';
 
+import { Fragment } from "react";
+import { Menu, Transition } from '@headlessui/react'
+import { MdEditDocument, MdDescription, MdDelete } from "react-icons/md";
+
+function classNames(...classes){
+    return classes.filter(Boolean).join(' ')
+}
+
 function PetCard({pet}){
     const { deletePet } = usePets();
 
@@ -19,35 +27,83 @@ function PetCard({pet}){
     return(
         <div className="bg-sky-100 w-full max-w-xs border border-cyan-600 rounded-2xl shadow-2xl">
             <div className="flex justify-end px-2 pt-2">
-                <button id="dropdownButton" data-dropdown-toggle="dropdown"
-                    className="inline-block text-cyan-600 dark:text-cyan-400 hover:bg-cyan-900 dark:hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-cyan-600 dark:focus:ring-cyan-900 rounded-lg text-sm p-1.5" type="button">
-                    <span className="sr-only">Open dropdown</span>
-                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
-                    </svg>
-                </button>
-                {/* Dropdown menu */}
-                <div id="dropdown" className="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                    <ul className="py-2" aria-labelledby="dropdownButton">
-                        <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export Data</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
-                        </li>
-                    </ul>
-                </div>
+                <Menu as="div" className="relative inline-block text-left">
+                    <div>
+                        <Menu.Button
+                            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-sky-100 px-3 py-2 font-Caveat text-base ring-1 ring-inset ring-cyan-600 hover:bg-cyan-600 hover:text-white">
+                            . . .
+                        </Menu.Button>
+                    </div>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items
+                            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-sky-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        >
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {
+                                        ({ active }) => (
+                                            <Link
+                                                to={'/pet/' + pet._id}
+                                                className={classNames(
+                                                    active ? 'bg-sky-200 font-Caveat text-xl' : 'font-Caveat text-xl',
+                                                    'block px-4 py-2 text-sm'
+                                                )}
+                                            >
+                                            <MdEditDocument className="h-5 w-8 inline-flex" />Editar Datos
+                                            </Link>
+                                        )
+                                    }
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {
+                                        ({ active }) => (
+                                            <Link
+                                                to={'/petsInfo/' + pet._id}
+                                                className={classNames(
+                                                    active ? 'bg-sky-200 font-Caveat text-xl' : 'font-Caveat text-xl',
+                                                    'block px-4 py-2 text-sm'
+                                                )}
+                                            >
+                                            <MdDescription className="h-5 w-8 inline-flex" />Completar Información
+                                            </Link>
+                                        )
+                                    }
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {
+                                        ({ active }) => (
+                                            <Link
+                                                onClick={()=>{deletePet(pet._id);}}
+                                                className={classNames(
+                                                    active ? 'bg-sky-200 font-Caveat text-xl' : 'font-Caveat text-xl',
+                                                    'block px-4 py-2 text-sm'
+                                                )}
+                                            >
+                                            <MdDelete className="h-5 w-8 inline-flex" />Eliminar
+                                            </Link>
+                                        )
+                                    }
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
             </div>
-            <div className="bg-sky-100 flex flex-col items-center pb-5">
+            <div className="bg-sky-100 flex flex-col items-center pb-5 rounded-2xl">
                 <img className="w-52 h-52 rounded-full shadow-lg" src={image} />
                 <h5 className="font-Caveat font-bold text-xl my-3">Nombre: {pet.name}</h5>
                 <span className="font-Caveat text-xl">Sexo: {sexo}</span>
                 <span className="font-Caveat text-xl">Fecha de Nacimiento: {pet.age_day} / {pet.age_month} / {pet.age_year}</span>
             </div>
-            <div className="flex gap-x-2 items-center justify-center px-6 pb-2">
+            {/* <div className="flex gap-x-2 items-center justify-center px-6 pb-2">
                 <Link to={'/pet/' + pet._id} className="bg-transparent hover:bg-cyan-600 text-sky-500
                     font-semibold hover:text-white mb-2 py-2 px-4 border border-cyan-600 hover:border-transparent rounded"
                 >Editar</Link>
@@ -62,7 +118,7 @@ function PetCard({pet}){
                 <Link to={'/petsInfo/' + pet._id} className="bg-transparent hover:bg-cyan-600 text-sky-500
                     font-semibold hover:text-white mb-2 py-2 px-4 border border-cyan-600 hover:border-transparent rounded"
                 >Completar Información</Link>
-            </div>
+            </div> */}
         </div>
     )
 }
